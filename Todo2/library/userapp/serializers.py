b3ba49ad462f43114cfd541092a219ffa8717a97
Treglaov <egort,123@yandex.ru>
2023-02-18 15:14:50 +0500

    .

--- Todo2/library/userapp/serializers.py
+++ Todo2/library/userapp/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 from .models import User
 
-class UsesModelSerializer(HyperlinkedModelSerializer):
+
+class UserModelSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ('username', 'first_name', 'last_name', 'email',)
